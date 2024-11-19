d8dbf1d8a1ede278b9cb00673ac921ac787ce1bb
Nguyen Huu Thanh <alannguyen.cs@gmail.com>
2024-11-19 15:11:37 +0800

    try to push to main which require pull request

---
+++ example.py
@@ -0,0 +1,14 @@
+"""Module for basic arithmetic operations."""
+
+
+def add_numbers(num1: float, num2: float) -> float:
+    """Add two numbers together.
+
+    Args:
+        num1 (float): First number to add
+        num2 (float): Second number to add
+
+    Returns:
+        float: Sum of the two numbers
+    """
+    return num1 + num2
